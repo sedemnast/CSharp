424dfea438f92f9f861f96777314acee18036ed5
Nataly <nzaitseva1799@gmail.com>
2022-12-22 04:54:20 +0300

    Решила задачу 8

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число, чтобы увидеть все четные числа от 1 до этого числа");
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int i = 2;
+
+while (i <= number)
+{
+    if (i % 2 == 0) 
+    {
+        WriteLine(i);
+        i = i + 2;
+    }
+}
