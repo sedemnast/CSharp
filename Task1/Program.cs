0dd6b03a96aa0a245a1ff78a188207f5c561b815
Nataly <nzaitseva1799@gmail.com>
2022-12-22 03:47:43 +0300

    Решили задачу 1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Является ли число a квадратом числа b?");
+Write("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+if(b * b == a)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
