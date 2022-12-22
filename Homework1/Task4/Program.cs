4f00bbf71e179421da00ac8d7f7e629121961da7
Nataly <nzaitseva1799@gmail.com>
2022-12-22 03:57:09 +0300

    Решила задачу 4

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите три числа, чтобы найти максимальное из них");
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+Write("Введите третье число: ");
+int c = int.Parse(ReadLine()!);
+
+int max = a;
+
+if (a > b && a > c)
+{
+    WriteLine($"max = {a}");
+}
+if (b > a && b > c)
+{
+    WriteLine($"max = {b}");
+}
+if (c > a && c > b)
+{
+    WriteLine($"max = {c}");
+}
