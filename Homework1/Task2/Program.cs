b62cb975c2d4a2c7d32d49f01667a0986ee48bd5
Nataly <nzaitseva1799@gmail.com>
2022-12-22 03:55:37 +0300

    Решила задачу 2

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите два числа, чтобы узнать, какое из них большее, а какое меньшее");
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+int max = a;
+int min = b;
+
+if (a > b)
+{
+    WriteLine($"max = {a}, min = {b}");
+}
+    else if (a < b)
+    { 
+        WriteLine($"max = {a}, min = {b}");
+    }
+else
+{
+    WriteLine($"Числа равны");
+}
