50ed0135edc2414354423770986151ebd9fd0764
Nataly <nzaitseva1799@gmail.com>
2022-12-22 04:10:40 +0300

    Решила задачу 6

--- Homework1/Task6/Program.cs
+++ Homework1/Task6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число, чтобы узнать, четное оно или нет");
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a % 2 == 0)
+{
+    WriteLine("Число четное");
+}
+else
+{
+    WriteLine("Число нечетное");
+}
