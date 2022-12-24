7d6ab05701af8cac21063228a720384f65434ea9
Nataly <nzaitseva1799@gmail.com>
2022-12-25 01:09:26 +0300

    Решили задачу 9

---
+++ Task9/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+int number = new Random().Next(10, 100);
+WriteLine(number);
+WriteLine(number / 10 > number % 10 ? number / 10 : number % 10);
+
+if(number / 10 > number % 10)
+{
+    WriteLine(number / 10);
+}
+else
+{
+    WriteLine(number % 10);
+}
