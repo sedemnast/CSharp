2d7ce67eee47af69d895a9132b366dd28250f8bc
Nataly <nzaitseva1799@gmail.com>
2022-12-25 23:47:54 +0300

    Решила задачу 13

--- Homework2/Task13/Program.cs
+++ Homework2/Task13/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число:");
+int number = Convert.ToInt32(ReadLine());
+
+if(number > 99)
+{
+    while (number > 999)
+        {
+            number /= 10;
+        }
+        int digit = number %= 10;
+        WriteLine(digit);
+}
+else
+{
+    WriteLine("Третьей цифры нет");
+}
+
+
