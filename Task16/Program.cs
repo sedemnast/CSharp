bd99692c7d480ce2acbcffdacbc61d154d710668
Nataly <nzaitseva1799@gmail.com>
2022-12-25 20:07:25 +0300

    Решили задачу 16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Является ли одно число квадратом другого?");
+Write("Введите число a: ");
+int a = Convert.ToInt32(ReadLine());
+Write("Введите число b: ");
+int b = Convert.ToInt32(ReadLine());
+
+WriteLine(a * a == b ? "b является квадратом числа a" : b*b == a ? "a является квадратом числа b" : "Не являются кдадратами друг друга");
