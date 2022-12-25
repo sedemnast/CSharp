3b9ca13a137205934d6aab8b2e75236f17be7be2
Nataly <nzaitseva1799@gmail.com>
2022-12-26 00:07:50 +0300

    Решила задачу 15

--- Homework2/Task15/Program.cs
+++ Homework2/Task15/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Является ли этот день выходным?");
+Write("Введите цифру, обозначающую день недели: ");
+int day = int.Parse(ReadLine()!);
+
+if(day < 6 & day > 0) 
+{
+    WriteLine("Нет, это рабочий день");
+}
+    else if(day >= 6 & day <= 7) 
+    {
+    WriteLine("Да, это выходной");
+    }
+else
+{
+    WriteLine("Такого дня недели не существует");
+}
