900f873b8bc6c31d92f6dca4ebd92c3e3bebd1cb
Nataly <nzaitseva1799@gmail.com>
2022-12-25 19:31:58 +0300

    Решили задачу 12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Является ли второе число кратным первому?");
+WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(ReadLine());
+//int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(ReadLine());
+//int b = int.Parse(ReadLine()!);
+
+WriteLine(a%b == 0?$"Число {a} кратно числу {b}":$"Число {a} не кратно числу {b}, остаток {a%b}");
+
+// if(a%b == 0)
+// {
+//     WriteLine($"Число {a} кратно числу {b}");
+// }
+// else
+// {
+//     WriteLine($"Число {a} не кратно числу {b}");
+//     Write($"Остаток {a%b}");
+// }
