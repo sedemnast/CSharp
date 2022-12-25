58385a044f65ab34a35c219be61eb2ebecb94d31
Nataly <nzaitseva1799@gmail.com>
2022-12-25 19:29:14 +0300

    Решили дополнительную задачу

--- AdditionalTask/Program.cs
+++ AdditionalTask/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(ReadLine());
+
+WriteLine(a == b ? "=" : a > b ? ">" : "<");
