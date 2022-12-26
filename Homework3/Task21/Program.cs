fc9a40b07c2c7278f629c6a7f843c8efe14a1b18
Nataly <nzaitseva1799@gmail.com>
2022-12-26 23:27:07 +0300

    Решила задачу 21

--- Homework3/Task21/Program.cs
+++ Homework3/Task21/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите координаты точки A");
+Write("Введите ax: ");
+int ax = int.Parse(ReadLine()!);
+Write("Введите ay: ");
+int ay = int.Parse(ReadLine()!);
+Write("Введите az: ");
+int az = int.Parse(ReadLine()!);
+
+WriteLine("Введите координаты точки B");
+Write("Введите bx: ");
+int bx = int.Parse(ReadLine()!);
+Write("Введите by: ");
+int by = int.Parse(ReadLine()!);
+Write("Введите bz: ");
+int bz = int.Parse(ReadLine()!);
+
+double length = Math.Sqrt(Math.Pow((bx-ax),2)+ Math.Pow((by-ay),2)+ Math.Pow((bz-az),2));
+WriteLine($"{length:f2}");
+
