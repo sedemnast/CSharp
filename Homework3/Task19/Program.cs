d0f5dc562376be076f19404dfbb81ec7f11df218
Nataly <nzaitseva1799@gmail.com>
2022-12-27 00:38:11 +0300

    Решила задачу 19

--- Homework3/Task19/Program.cs
+++ Homework3/Task19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Является ли Ваше пятизначное число палиндромом?");
+Write("Введите пятизначное число: ");
+bool isParse = int.TryParse(ReadLine(), out int n);
+if(!isParse)
+{
+    WriteLine("Введено не число");
+    return;
+}
+
+if (n > 99999 || n < 10000)
+{
+    WriteLine("Вы ввели не пятизначное число");
+}
+    else if (n / 10000 == n % 10 && (n / 1000) % 10 == (n / 10) % 10)
+    {
+        WriteLine($"Число {n} является палиндромом");
+    }
+    else
+    {
+        WriteLine($"Число {n} не является палиндромом");  
+    }
