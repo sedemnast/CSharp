3ef097633da87d7347482455417cb885044adecc
Nataly <nzaitseva1799@gmail.com>
2022-12-26 23:16:58 +0300

    Решили задачу 22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число, чтобы получить таблицу квадратов чисел от 1 до числа: ");
+bool isParse = int.TryParse(ReadLine(), out int n);
+if(!isParse)
+{
+    WriteLine("Введено не число");
+    return;
+}
+int count = 1;
+while (count <= n)
+{
+    WriteLine($"Квадрат числа {count} - {Math.Pow(count,2)}");
+    count++;
+}
