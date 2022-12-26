6cf50662cfda8832bb7117e7573ff8d3c7fd162d
Nataly <nzaitseva1799@gmail.com>
2022-12-26 23:38:32 +0300

    Решила задачу 23

--- Homework3/Task23/Program.cs
+++ Homework3/Task23/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число, чтобы получить таблицу кубов чисел от 1 до числа: ");
+bool isParse = int.TryParse(ReadLine(), out int n);
+if(!isParse)
+{
+    WriteLine("Введено не число");
+    return;
+}
+int count = 1;
+while (count <= n)
+{
+    WriteLine($"Куб числа {count} - {Math.Pow(count,3)}");
+    count++;
+}
