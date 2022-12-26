30598d3cee49bc746ae7a1554d66e9aa33f923c3
Nataly <nzaitseva1799@gmail.com>
2022-12-26 21:31:58 +0300

    Решили задачу 3 с помощью switch-case

--- Task3_SwitchCase/Program.cs
+++ Task3_SwitchCase/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите номер дня недели:");
+int day = int.Parse(ReadLine()!);
+
+switch(day)
+{
+    case 1:
+    {
+        WriteLine("Понедельник");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("Вторник");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("Среда");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("Четверг");
+        break;
+    }
+     case 5:
+    {
+        WriteLine("Пятница");
+        break;
+    }
+     case 6:
+    {
+        WriteLine("Суббота");
+        break;
+    }
+     case 7:
+    {
+        WriteLine("Воскресенье");
+        break;
+    }
+    default:
+    {
+        WriteLine("Такого дня недели не существует");
+        break;
+    }
+}
