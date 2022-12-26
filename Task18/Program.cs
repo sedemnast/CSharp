5e880fd7ca2d65cb3cbbb97536eff90aa0a50ffa
Nataly <nzaitseva1799@gmail.com>
2022-12-26 21:31:05 +0300

    Решили задачу 18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите координату x: ");
+int x = int.Parse(ReadLine()!);
+Write("Введите координату y: ");
+int y = int.Parse(ReadLine()!);
+
+if(x == 0 || y == 0)
+{
+    WriteLine("Ошибка");
+    return;
+}
+if (x > 0 && y > 0)
+{
+    WriteLine("1 четверть");
+}
+if (x < 0 && y > 0)
+{
+    WriteLine("2 четверть");
+}
+if (x < 0 && y < 0)
+{
+    WriteLine("3 четверть");
+}
+if (x > 0 && y < 0)
+{
+    WriteLine("4 четверть");
+}
