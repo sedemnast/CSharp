5bc914363540bd4262d21dbb7b785962c3484eac
Nataly <nzaitseva1799@gmail.com>
2022-12-26 22:47:14 +0300

    Решили задачу 20

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите координаты точки A");
+Write("Введите ax: ");
+int ax = int.Parse(ReadLine()!);
+Write("Введите ay: ");
+int ay = int.Parse(ReadLine()!);
+
+WriteLine("Введите координаты точки B");
+Write("Введите bx: ");
+int bx = int.Parse(ReadLine()!);
+Write("Введите by: ");
+int by = int.Parse(ReadLine()!);
+
+double length = Math.Sqrt(Math.Pow((bx-ax),2)+ Math.Pow((by-ay),2));
+WriteLine($"{length:f2}");
+
