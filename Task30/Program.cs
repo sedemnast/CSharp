4278c29ef74dc25b67408ec8eed2a632298e83a7
Nataly <nzaitseva1799@gmail.com>
2023-01-19 15:39:38 +0300

    Решили задачу 30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выдает сумму чисел от 1 до N, на которые N делится без остатка.
+
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSum(number);
+WriteLine($"Сумма чисел от 1 до N, на которые N делится без остатка = {sum}");
+
+int GetSum(int N)
+{
+    int result = 0;
+    int temp = N;
+    while (N > 0)
+    {
+        if (temp % N == 0)
+        {
+        result += N;
+        }
+        N--;
+    }
+
+    return result;
+}
