989ff76482ea9c28b8f9e4d4f76c7c923a58b970
Nataly <nzaitseva1799@gmail.com>
2023-01-19 17:11:28 +0300

    Решила задачу 27

--- Homework4/Task27/Program.cs
+++ Homework4/Task27/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = SumDigits(number);
+WriteLine($"Сумма цифр в числе = {sum}");
+
+int SumDigits(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10; 
+        num /= 10; 
+    }
+    return result;
+}
