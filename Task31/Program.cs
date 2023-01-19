c0756cac9b94308911ef717f0c5d6dcbcf6f2841
Nataly <nzaitseva1799@gmail.com>
2023-01-19 16:34:27 +0300

    Решили задачу 31

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+using static System.Console;
+Clear();
+
+int[] arr = GetArray(8);
+
+PrintArray(arr);
+
+void FullArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+int [] GetArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(2);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    int length = array.Length; 
+    for (int i = 0; i < length; i++) 
+    {
+        Write($"{array[i]} "); 
+    }
+    WriteLine();
+}
