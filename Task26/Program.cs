9774679d4fbfe478d9eaadea29a40b785480f360
Nataly <nzaitseva1799@gmail.com>
2023-01-19 15:38:10 +0300

    Решили задачу 26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую метод, который принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+using static System.Console;
+
+Clear();
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int product = GetProduct(number);
+WriteLine($"Product number 1..N = {product}");
+
+int GetProduct(int N)
+{
+    if (N <= 0) return 0;
+    int result = 1;
+    while (N > 0)
+    {
+        result *= N;
+        N--;
+    }
+
+    return result;
+}
