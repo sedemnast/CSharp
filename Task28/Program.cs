bc4f470eb3183b63dc9add7ced1ca441a843e5bf
Nataly <nzaitseva1799@gmail.com>
2023-01-19 15:38:58 +0300

    Решили задачу 28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int count = NumCount(number);
+WriteLine($"Количество цифр в числе = {count}");
+
+int NumCount(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result++;
+        num/=10;
+    }
+
+    return result;
+}
