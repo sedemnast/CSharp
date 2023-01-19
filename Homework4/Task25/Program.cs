9f0eba89d86d35f56ef4bbcbbb8f4649ae6cf8b0
Nataly <nzaitseva1799@gmail.com>
2023-01-19 16:57:33 +0300

    Решила задачу 25

--- Homework4/Task25/Program.cs
+++ Homework4/Task25/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using static System.Console;
+Clear();
+
+Write("Введите число A: ");
+int A = Convert.ToInt32(ReadLine());
+Write("Введите число B: ");
+int B = Convert.ToInt32(ReadLine());
+int exponentiation = Exponentiation(A, B);
+
+int Exponentiation(int A, int B)
+{
+  int result = 1;
+  for(int i = 1; i <= B; i++)
+  {
+    result *= A;
+  }
+  return result;
+}
+WriteLine($"{A} в натуральной степени {B} = {exponentiation}");
