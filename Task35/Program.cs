400acccb2c5b800987251bc5f5c40d79c6f26203
Nataly <nzaitseva1799@gmail.com>
2023-01-23 01:22:48 +0300

    Решили задачу 35

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+using static System.Console;
+
+Clear();
+
+int[] array = GetArray(123, -100, 100);
+
+PrintArray(array);
+WriteLine();
+WriteLine(ArrayCount(array));
+
+int ArrayCount(int[] inArr)
+{
+    int result = 0;
+    foreach (var i in inArr)
+    {
+        if (10 <= i && i <= 99) result++;
+    }
+    return result;
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] resultArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
