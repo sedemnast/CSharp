47231c84cf6e852a2f24a48c7e2b6736697b77c3
Nataly <nzaitseva1799@gmail.com>
2023-01-23 03:30:19 +0300

    Решила задачу 38

--- Homework5/Task38/Program.cs
+++ Homework5/Task38/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую методы формирования массива, 
+// заполненного случайными **вещественными** числами и
+// вычисления разницы между максимальным и минимальным элементов массива.
+// [3,3 7,1 22,5 2,2 78,2] -> 76
+
+using static System.Console;
+
+Clear();
+Write("Введите размер массива, min и max через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+double[] array = GetArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]));
+
+PrintArray(array);
+WriteLine();
+Diff(array);
+
+void Diff(double[] inArray)
+{
+    double min = double.MaxValue;
+    double max = double.MinValue;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] > max) max = inArray[i];
+        if (inArray[i] < min) min = inArray[i];
+    }
+    WriteLine($"Максимальный элемент = {max}");
+    WriteLine($"Минимальный элемент = {min}");
+    WriteLine($"Разница между максимальным и минимальным элементами массива = {max - min}");
+}
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] resultArray = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = Math.Round(rnd.Next(minValue, maxValue) + rnd.NextDouble(), 2);
+    }
+    return resultArray;
+}
+
+void PrintArray(double[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
