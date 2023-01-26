3e9b062a2420126905b0629d79b271f9e2e85d55
Nataly <nzaitseva1799@gmail.com>
2023-01-26 17:20:17 +0300

    Решила задачу 41

--- Homework6/Task41/Program.cs
+++ Homework6/Task41/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+Write("Введите числа через пробел: ");
+int[] numbers = Array.ConvertAll(ReadLine().Split(), int.Parse);
+
+WriteLine($"Количество чисел больше 0 = {SumNum(numbers)}");
+
+int SumNum (int[] num)
+{
+    int sum = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] > 0) sum++;
+    }
+    return sum;
+}
+
