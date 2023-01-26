01f198b8f0529ed6c5a11c996c9cdd54da1d7c70
Nataly <nzaitseva1799@gmail.com>
2023-01-26 16:06:19 +0300

    Решили задачу 45

--- Task45/Program.cs
+++ Task45/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+using static System.Console;
+Clear();
+
+int[] array = { 1, 2, 6, 7, 3 };
+WriteLine($"Копия заданного массива = [{String.Join(", ", CopyArray(array))}]");
+
+int[] CopyArray(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[i];
+    }
+    return result;
+}
+
