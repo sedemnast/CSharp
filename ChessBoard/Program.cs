bda9a49c8a80e2a2135bedf44025e0e9745dc40f
Nataly <nzaitseva1799@gmail.com>
2023-01-30 15:47:07 +0300

    Решили задачу про шахматную доску

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Самая первая клетка шахматной доски - черная, далее они чередуются.
+// Пользователь вводит координаты клетки.
+// Какого цвета эта клетка?
+
+using static System.Console;
+
+Clear();
+
+Write("Введите координаты клетки : ");
+
+string parameters = ReadLine();
+int par1 = parameters[0];
+int par2 = parameters[1];
+
+WriteLine(par1);
+WriteLine(par2);
+
+if ((par1 + par2) % 2 == 0)
+{
+    WriteLine("Черная");
+}
+else
+{
+    WriteLine("Белая");
+}
