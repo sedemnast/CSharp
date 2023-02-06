eea65e3f6fb9cb0516a81d87b65f4005dd2b8c93
Nataly <nzaitseva1799@gmail.com>
2023-02-06 12:58:11 +0300

    Решила задачу 66

--- Homework9/Task66/Program.cs
+++ Homework9/Task66/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая реализует рекурсивный метод нахождения суммы 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using static System.Console;
+
+Clear();
+
+WriteLine(GetSum(1, 15));
+
+int GetSum(int numM, int numN)
+{
+    if (numM == numN) return numN;
+    else return GetSum(numM + 1, numN) + numM;
+}
