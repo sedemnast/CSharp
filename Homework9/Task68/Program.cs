e78def585cfcba590d489958bb64d8914dcfd9e3
Nataly <nzaitseva1799@gmail.com>
2023-02-06 13:25:13 +0300

    Решила задачу 68

--- Homework9/Task68/Program.cs
+++ Homework9/Task68/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу реализующую метод вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using static System.Console;
+
+Clear();
+
+WriteLine(AckermannFunction(2, 3));
+
+int AckermannFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0 && m > 0) return AckermannFunction (m - 1, 1);
+    if (n > 0 && m > 0) return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    return 0;
+}
