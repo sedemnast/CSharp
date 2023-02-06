0ddba37026adbcfe8c031b342d6e557ec8f51e02
Nataly <nzaitseva1799@gmail.com>
2023-02-06 12:49:40 +0300

    Решила задачу 64

--- Homework9/Task64/Program.cs
+++ Homework9/Task64/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая реализует рекурсивный метод вывода всех натуральных 
+// числел в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using static System.Console;
+
+Clear();
+
+WriteLine(GetNumbers(8));
+
+string GetNumbers(int num)
+{
+    if (num == 1) return num.ToString();
+    else return $"{num}, {GetNumbers(num-1)}";
+}
