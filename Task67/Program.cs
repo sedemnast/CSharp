cb7d9b7f8cb392d554add611fcf8dfa157837ae8
Nataly <nzaitseva1799@gmail.com>
2023-02-06 12:40:55 +0300

    Решили задачу 67

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+using static System.Console;
+
+Clear();
+
+WriteLine(GetSum(45));
+
+int GetSum(int num)
+{
+    if (num == 0) return 0; 
+    else return GetSum(num / 10) + num % 10;
+}
