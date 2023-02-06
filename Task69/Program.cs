d49ff3a0247650a574e1e061a41e1fd3296e3b8c
Nataly <nzaitseva1799@gmail.com>
2023-02-06 12:41:30 +0300

    Решили задачу 69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+using static System.Console;
+
+Clear();
+
+WriteLine(Exponentiation(2, 3));
+
+int Exponentiation(int num, int pow)
+{
+    if (pow == 1) return num;
+    else return Exponentiation(num, pow - 1) * num;
+}
